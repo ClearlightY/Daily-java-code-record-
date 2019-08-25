a7e7e83a8c84df5bd3468f172c2e16ecaccb7603
lxy <clearlight@yeah.net>
2019-08-25 14:55:48 +0800

    upload java foundation code

---
+++ 传智Video/src/top/clearlight/question/IntegerTest.java
@@ -0,0 +1,9 @@
+package top.clearlight.question;
+
+public class IntegerTest {
+    public static void main(String[] args) {
+        Integer i = 120;
+        Integer j = 120;
+        System.out.println(i == j);
+    }
+}

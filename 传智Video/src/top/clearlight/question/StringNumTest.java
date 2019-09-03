1181cd4971388995797f027b8a1b9bf1965f53cb
lixiaoyi <clearlight@yeah.net>
2019-09-03 21:29:38 +0800

    add methods , for reflect create test

---
+++ 传智Video/src/top/clearlight/question/StringNumTest.java
@@ -0,0 +1,10 @@
+package top.clearlight.question;
+
+public class StringNumTest {
+    public static void main(String[] args) {
+        String s = "a";
+        s = s + "b" + "c";
+        s = s + "b" + "c" + "d";
+        System.out.println(s);
+    }
+}

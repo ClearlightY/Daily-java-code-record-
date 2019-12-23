925a095f6db1c1e685c24fab7d572512dd0196a8
lxy <lxy12531@163.com>
2019-12-23 19:33:00 +0800

    Java_Web learn include Cookie Session and LoginServlet

---
+++ 传智Video/src/top/clearlight/question/FundamentalQuestion.java
@@ -0,0 +1,17 @@
+package top.clearlight.question;
+
+public class FundamentalQuestion {
+    public static void main(String[] args) {
+        int i = 0;
+        for (int j = 0; j < 50; j++) {
+            i = i++;
+        }
+        System.out.println(i);
+
+        // partVar().a;
+    }
+
+    public static void partVar() {
+        int a = 1;
+    }
+}

938329b26152960067b1bbca7dc4b59e06431a27
lxy <lxy12531@163.com>
2019-12-24 10:35:00 +0800

    Java runtime operation process

--- 传智Video/src/top/clearlight/question/FundamentalQuestion.java
+++ 传智Video/src/top/clearlight/question/FundamentalQuestion.java
@@ -1,5 +1,7 @@
 package top.clearlight.question;
 
+import org.junit.jupiter.api.Test;
+
 public class FundamentalQuestion {
     public static void main(String[] args) {
         int i = 0;
@@ -14,4 +16,26 @@ public class FundamentalQuestion {
     public static void partVar() {
         int a = 1;
     }
+
+    @Test
+    public void test() {
+        System.out.println(S.abc);
+    }
+
 }
+
+
+class P {
+    public static int abc = 123;
+
+    static {
+        System.out.println("P is init");
+    }
+}
+
+class S extends P {
+    static {
+        System.out.println("S is init");
+    }
+}
+

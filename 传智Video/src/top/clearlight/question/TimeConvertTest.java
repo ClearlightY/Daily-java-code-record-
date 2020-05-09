793f6355b821e28381f5f1ba2492c2afbb0e00df
lxy <lxy12531@163.com>
2020-05-09 18:21:12 +0800

    time show problem

---
+++ 传智Video/src/top/clearlight/question/TimeConvertTest.java
@@ -0,0 +1,14 @@
+package top.clearlight.question;
+
+import java.util.Date;
+
+/**
+ * @author <a href="mail to: lxy12531@163.com" rel="nofollow">lxy</a>
+ * @date 2020/5/8 20:27
+ */
+public class TimeConvertTest {
+    public static void main(String[] args) {
+        long time = new Date().getTime();
+        System.out.println(time);
+    }
+}
